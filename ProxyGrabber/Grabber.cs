8788e1c54ecd45a57fdb5aa32e3a8e881d6d1d62
H4CK3ROFF <yaroslav.donetsk@gmail.com>
2021-10-29 18:32:27 +0300

    main

---
+++ ProxyGrabber/Grabber.cs
@@ -0,0 +1,81 @@
+﻿using Leaf.xNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace ProxyGrabber
+{
+    internal class Grabber
+    {
+
+        #region Variables
+
+        private string[] links = new string[] {
+            "https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt",
+            "https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt",
+            "https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt",
+            "https://raw.githubusercontent.com/fate0/proxylist/master/proxy.list",
+            "https://raw.githubusercontent.com/a2u/free-proxy-list/master/free-proxy-list.txt",
+            "http://rootjazz.com/proxies/proxies.txt",
+            "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all",
+            "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks5&timeout=10000&country=all&ssl=all&anonymity=all",
+            "https://github.com/roosterkid/openproxylist/blob/main/SOCKS5_RAW.txt",
+            "https://github.com/roosterkid/openproxylist/blob/main/HTTPS_RAW.txt",
+            "https://github.com/opsxcq/proxy-list/blob/master/list.txt",
+            "https://github.com/hookzof/socks5_list/blob/master/proxy.txt",
+            "https://github.com/ShiftyTR/Proxy-List/blob/master/proxy.txt",
+            "https://github.com/clarketm/proxy-list/blob/master/proxy-list.txt",
+            "https://github.com/mmpx12/proxy-list/blob/master/proxies.txt",
+            "https://github.com/Volodichev/proxy-list/blob/main/hproxy.txt",
+            "https://github.com/Volodichev/proxy-list/blob/main/http.txt",
+            "https://github.com/Volodichev/proxy-list/blob/main/http_old.txt",
+            "https://github.com/sunny9577/proxy-scraper/blob/master/proxies.txt",
+            "https://github.com/jetkai/proxy-list/blob/main/online-proxies/txt/proxies.txt",
+            "https://checkerproxy.net/api/archive/" + DateTime.Today.ToString("yyyy-MM-d"),
+        };
+
+        public delegate void SetProxy(string data);
+        public event SetProxy setProxy;
+
+        #endregion
+
+        #region Functions
+
+        public void GetProxy()
+        {
+            string result = "";
+            HttpRequest data = new HttpRequest();
+            foreach (string link in links)
+            {
+                string response = data.Get(link).ToString();
+                result += GetAllProxyFromResponse(response).ToString();
+            }
+            setProxy(result);
+        }
+
+        private string GetAllProxyFromResponse(string response)
+        {
+            string result = "";
+            Regex regex = new Regex(@"\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}:\d{1,5}");
+            MatchCollection matchCollection = regex.Matches(response);
+            if (matchCollection.Count > 0)
+            {
+                foreach (Match match in matchCollection)
+                {
+                    if (!match.Value.Any(char.IsLetter))
+                    {
+                        result += match.Value + "\n";
+                    }
+                }
+            }
+            return result;
+        }
+
+        #endregion
+
+    }
+}
---
+++ ProxyGrabber/MainWindow.xaml.cs
@@ -0,0 +1,175 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProxyGrabber
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        #region Variables
+        
+        private SolidColorBrush _titleButton = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF202020"));
+        private SolidColorBrush _titleButtonClickDown = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF5D5D5D"));
+        private SolidColorBrush _titleButtonMouseEnter = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF383838"));
+        private SolidColorBrush _titleCloseButtonClickDown = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFF0000"));
+
+        private SolidColorBrush _proxyButton = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF111111"));
+        private SolidColorBrush _proxyButtonClickDown = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF303030"));
+        private SolidColorBrush _proxyButtonMouseEnter = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF212121"));
+
+        private Grabber _grabber = new Grabber();
+
+        #endregion
+
+
+        #region Title
+        private void TitleBoardClickDown(object sender, RoutedEventArgs e)
+        {
+            this.DragMove();
+        }
+        #endregion
+
+
+        #region MinimizeButton
+        private void MinimizeButtonClickUp(object sender, RoutedEventArgs e)
+        {
+            MinimizeButton.Background = _titleButton;
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void MinimizeButtonClickDown(object sender, RoutedEventArgs e)
+        {
+            MinimizeButton.Background = _titleButtonClickDown;
+        }
+
+        private void MinimizeButtonMouseEnter(object sender, RoutedEventArgs e)
+        {
+            MinimizeButton.Background = _titleButtonMouseEnter;
+        }
+
+        private void MinimizeButtonMouseLeave(object sender, RoutedEventArgs e)
+        {
+            MinimizeButton.Background = _titleButton;
+        }
+        #endregion
+
+
+        #region CloseButton
+        private void CloseButtonClickUp(object sender, RoutedEventArgs e)
+        {
+            CloseButton.Background = _titleButton;
+            this.Close();
+        }
+
+        private void CloseButtonClickDown(object sender, RoutedEventArgs e)
+        {
+            CloseButton.Background = _titleCloseButtonClickDown;
+        }
+
+        private void CloseButtonMouseEnter(object sender, RoutedEventArgs e)
+        {
+            CloseButton.Background = _titleButtonMouseEnter;
+        }
+
+        private void CloseButtonMouseLeave(object sender, RoutedEventArgs e)
+        {
+            CloseButton.Background = _titleButton;
+        }
+        #endregion
+
+
+        #region GetProxyButton
+        private void GetProxyButtonClickUp(object sender, RoutedEventArgs e)
+        {
+            GetProxyButton.Background = _proxyButton;
+            _grabber.setProxy += SetProxy;
+            new Thread(() => _grabber.GetProxy()).Start();
+        }
+
+        private void GetProxyButtonClickDown(object sender, RoutedEventArgs e)
+        {
+            GetProxyButton.Background = _proxyButtonClickDown;
+        }
+
+        private void GetProxyButtonMouseEnter(object sender, RoutedEventArgs e)
+        {
+            GetProxyButton.Background = _proxyButtonMouseEnter;
+        }
+
+        private void GetProxyButtonMouseLeave(object sender, RoutedEventArgs e)
+        {
+            GetProxyButton.Background = _proxyButton;
+        }
+        #endregion
+
+
+        #region SaveProxyButton
+        private void SaveProxyButtonClickUp(object sender, RoutedEventArgs e)
+        {
+            SaveProxyButton.Background = _proxyButton;
+            SaveProxy();
+        }
+
+        private void SaveProxyButtonClickDown(object sender, RoutedEventArgs e)
+        {
+            SaveProxyButton.Background = _proxyButtonClickDown;
+        }
+
+        private void SaveProxyButtonMouseEnter(object sender, RoutedEventArgs e)
+        {
+            SaveProxyButton.Background = _proxyButtonMouseEnter;
+        }
+
+        private void SaveProxyButtonMouseLeave(object sender, RoutedEventArgs e)
+        {
+            SaveProxyButton.Background = _proxyButton;
+        }
+        #endregion
+
+
+        #region UpdateProxyList
+
+        private void SetProxy(string data)
+        {
+            Application.Current.Dispatcher.Invoke(new Action(() => ProxyList.Text = data));
+        }
+
+        #endregion
+
+
+        #region SaveProxy
+        private void SaveProxy()
+        {
+            string fileText = ProxyList.Text;
+            SaveFileDialog dialog = new SaveFileDialog() { Filter = "Text Files(*.txt)|*.txt|All(*.*)|*" };
+            if (dialog.ShowDialog() == true)
+                File.WriteAllText(dialog.FileName, fileText);
+        }
+        #endregion
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
